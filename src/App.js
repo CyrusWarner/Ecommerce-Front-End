1346bcf71647ed14286e0925e29bba4d706b881a
CyrusWarner <cyruswarner27@gmail.com>
2021-08-11 14:07:11 -0500

    Initial files.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';  
+import {BroswerRouter as Router, Switch, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div>
+      <Switch>
+        {/* <Route path="/" exact render={props => <COMPONENTNAMEHERE {...props} PASSINFOHERE={"SOMETHING HERE"}}></Route> */}
+        {/* <Route path="/" exact render={props => <COMPONENTNAMEHERE {...props} PASSINFOHERE={"SOMETHING HERE"}}></Route> */}
+        {/* <Route path="/" exact render={props => <COMPONENTNAMEHERE {...props} PASSINFOHERE={"SOMETHING HERE"}}></Route> */}
+        {/* <Route path="/" exact render={props => <COMPONENTNAMEHERE {...props} PASSINFOHERE={"SOMETHING HERE"}}></Route> */}
+        {/* <Route path="/" exact render={props => <COMPONENTNAMEHERE {...props} PASSINFOHERE={"SOMETHING HERE"}}></Route> */}
+        {/* <Route path="/" exact render={props => <COMPONENTNAMEHERE {...props} PASSINFOHERE={"SOMETHING HERE"}}></Route> */}
+      </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
