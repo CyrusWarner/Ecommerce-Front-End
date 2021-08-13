7d87a68291028c8f07504b416e18da03402859fb
Zachary <taylorzw96@gmail.com>
2021-08-13 17:09:38 -0500

    Added styling to NavBar

--- src/Components/NavigationBar/navigationBar.jsx
+++ src/Components/NavigationBar/navigationBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';  
 import { Link } from 'react-router-dom';
-import { Navbar,Nav, Form  } from 'react-bootstrap';
+import { Navbar,Nav, Form, Container  } from 'react-bootstrap';
+import './navigationBar.css';
+
 const NavigationBar = (props) => {
       let currentUser = props.currentUser
       const logout = () => {
@@ -9,37 +11,33 @@ const NavigationBar = (props) => {
         window.location.href = "/login";
       }
     return (
-        <Navbar bg="light" expand="lg">
-  <Navbar.Brand className="ms-2">Star Wars Ecommerce</Navbar.Brand>
-  <Navbar.Toggle aria-controls="navbarScroll" />
-  <Navbar.Collapse id="navbarScroll">
-    <Nav
-      className="mr-auto my-2 my-lg-0"
-      style={{ maxHeight: '100px' }}
-        navbarScroll
-    >
-      <Nav.Link as={Link} to="/" >Home</Nav.Link>
-      <Nav.Link as={Link} to="/products">View Products</Nav.Link>
-      <Nav.Link as={Link} to="/user/shoppingcart">Shopping Cart</Nav.Link>
-      <Nav.Link as={Link} to="/user/createproduct">Sell A Product</Nav.Link>
-      {currentUser &&
-      <Nav.Link as={Link} onClick={logout}>Logout</Nav.Link> 
-      }
-      {!currentUser &&
-      <Nav.Link as={Link} to="/Signup">Signup</Nav.Link>
-      }
-      {!currentUser &&
-      <Nav.Link as={Link} to="/Login">Login</Nav.Link>
-      }
-    </Nav>
-    <Form className="d-flex">
+        <Navbar className="color-nav">
+        {/* <Navbar.Brand>Star Wars Ecommerce</Navbar.Brand> */}
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav style={{ maxHeight: '100px' }} navbarScroll >
+            <Nav.Link as={Link} to="/" ><h4 className="linkText">Home</h4></Nav.Link>
+            <Nav.Link as={Link} to="/products"><h4 className="linkText">View Products</h4></Nav.Link>
+            <Nav.Link as={Link} to="/user/shoppingcart"><h4 className="linkText">Shopping Cart</h4></Nav.Link>
+            <Nav.Link as={Link} to="/user/createproduct"><h4 className="linkText">Sell A Product</h4></Nav.Link>
+            {currentUser &&
+            <Nav.Link as={Link} onClick={logout}><h4 className="linkText">Logout</h4></Nav.Link> 
+            }
+            {!currentUser &&
+            <Nav.Link as={Link} to="/Signup"><h4 className="linkText">Signup</h4></Nav.Link>
+            }
+            {!currentUser &&
+            <Nav.Link as={Link} to="/Login"><h4 className="linkText">Login</h4></Nav.Link>
+            }
+          </Nav>
+          <Form className="d-flex">
 
-    </Form>
-    <Nav>
-   
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+          </Form>
+          <Nav>
+        
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     )
 }
 
