cd96c3498974ebc4a2ffe064b821a540a1c5c542
CyrusWarner <cyruswarner27@gmail.com>
2021-08-12 19:58:30 -0500

    Added functionality for choosing rating with stars and not integers.

--- src/Components/ShowAllReviews/showAllReviews.jsx
+++ src/Components/ShowAllReviews/showAllReviews.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
+import { FaStar } from "react-icons/fa";
 const ShowAllReviews = (props) => {
   let productReviews = props.productReviews;
   let currentUser = props.currentUser.user
+  const stars = Array(5).fill(0);
   console.log(currentUser)
   return (
     <div>
@@ -14,7 +16,12 @@ const ShowAllReviews = (props) => {
               <Col sm={8}>
                   <h5>{currentUser.username}</h5>
                 <p>{review.description}</p>
-                <p>Rating: {review.rating}</p>
+                {}
+                
+                {stars.map((star, index) => index < review.rating && (
+                    <FaStar color={starColors.orange}/>
+                )  
+                )}
               </Col>
               <Col sm={4}></Col>
             </Row>
@@ -25,3 +32,15 @@ const ShowAllReviews = (props) => {
   );
 };
 export default ShowAllReviews;
+const starColors = {
+    orange: "#FFBA5A",
+    grey: "#a9a9a9",
+  };
+
+  const styles = {
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      // alignItems: "center"
+    },
+  };
