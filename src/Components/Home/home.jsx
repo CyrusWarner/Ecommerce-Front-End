fc305943f190425151eeb32449950801aae84a08
Zachary <taylorzw96@gmail.com>
2021-08-17 17:30:08 -0500

    Centered text on title for home page

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -15,9 +15,9 @@ const Home = () => {
           <div class="title">
             <p class="title">Welcome to Star Wars eCommerce!</p>
             <h1>In our store you can find merchandise related to Star Wars</h1>
-          </div>
           <p>Episode IV</p>
           <p>A New Hope</p>
+          </div>
           <p>It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.</p>     
         <p>During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.</p>
         <p>Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy…</p>
