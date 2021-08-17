f5d28ec13f1f31d8697fa8fd7a276dc78d7ccbed
Andy Wade <carrollandywadejr@gmail.com>
2021-08-17 12:35:58 -0500

    added a average star rating in the showProduct

--- src/Components/ShowProduct/showProduct.jsx
+++ src/Components/ShowProduct/showProduct.jsx
@@ -3,11 +3,13 @@ import ReviewForm from "../ReviewForm/reviewForm";
 import { Col, Container, Row, Button } from "react-bootstrap";
 import ShowAllReviews from './../ShowAllReviews/showAllReviews';
 import "./showProduct.css";
+import { FaStar } from "react-icons/fa";
 const ShowProduct = (props) => {
   const { name, description, price, averageRating } = props.currentProduct;
   const currentUser = props.currentUser;
   const currentToken = props.currentToken;
   const getProductReviews = props.getProductReviews
+  const stars = Array(5).fill(0);
   let productReviews = props.productReviews;
   return (
     <React.Fragment>
@@ -21,7 +23,10 @@ const ShowProduct = (props) => {
             <h2>${price}</h2>
             <h3>Category:{props.currentProduct.category.categoryName}</h3>
             <h4>Description:{description}</h4>
-            <p>Average Review:{averageRating} </p>
+            <p>{stars.map((star, index) => index < averageRating && (
+                      <FaStar/>
+                      )  
+                      )}</p>
             <Button onClick={() => props.addItemToCart(props.currentProduct)} style={{
                         backgroundColor: "crimson",
                         borderColor: "crimson",
