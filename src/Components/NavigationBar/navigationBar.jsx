ca5b5e8f39d0ab1f3cb2531933793b6e9ca9ef2e
CyrusWarner <cyruswarner27@gmail.com>
2021-08-17 15:52:19 -0500

    Fixed navbar from not showing as sticky.

--- src/Components/NavigationBar/navigationBar.jsx
+++ src/Components/NavigationBar/navigationBar.jsx
@@ -6,7 +6,7 @@ import './navigationBar.css';
 const NavigationBar = (props) => {
   const {currentUser, getUsersCart, logout, getUsersProducts} = props
     return (
-        <Navbar className="color-nav"  expand="lg">
+        <Navbar className="color-nav" sticky="top" expand="lg">
   <Container className="h-100">
     {currentUser &&
     <Navbar.Brand > <h4 className="linkText">Welcome {currentUser.user.username}!</h4></Navbar.Brand>
